cbf8ea494fba8a81226ec9ebf2e9f49ab7252484
XeniaP <phoebe93xenia@gmail.com>
2019-08-15 19:28:03 -0500

    trend

--- scAPI.py
+++ scAPI.py
@@ -5,7 +5,6 @@ import sys
 import urllib3
 
 #environmental variables
-"""
 imagetag=os.environ.get("IMAGETAG")
 buildid=os.environ.get("BUILD_ID")
 high_t=os.environ.get("HIGH")
@@ -15,18 +14,6 @@ negligible_t=os.environ.get("NEGLIGIBLE")
 unknown_t=os.environ.get("UNKNOWN")
 user=os.environ.get("USER")
 password=os.environ.get("PASSWORD")
-"""
-imagetag='apache'
-buildid='12'
-high_t=1
-medium_t=5
-low_t=1
-negligible_t=10
-unknown_t=5
-user='administrator'
-password='Trendmicr0!'
-
-layers=[]
 
 def requestToken():
     url = "https://a0def78f6bfa011e99b471244930895f-1322482254.us-east-1.elb.amazonaws.com/api/sessions"
@@ -45,10 +32,10 @@ def requestScan():
     url = "https://a0def78f6bfa011e99b471244930895f-1322482254.us-east-1.elb.amazonaws.com/api/scans"
     data = {"source": {
         "type": "docker",
-        "registry": "https://946007956850.dkr.ecr.us-west-2.amazonaws.com/test/apachestruts",
+        "registry": "https://786395520305.dkr.ecr.us-west-2.amazonaws.com",
         "repository": "test/apachestruts",
         "tag": 'latest',
-        "credentials": {"aws": {"region": "us-east-2"}}},
+        "credentials": {"aws": {"region": "us-west-2"}}},
         "webhooks": [{
         "hookURL": createWebHook()}]}
     headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer'+requestToken(), 'X-API-Version': '2018-05-01'}
@@ -60,8 +47,8 @@ def requestScan():
     return response.json()['id']
 
 def sendToSlack(message):
-    url = 'https://hooks.slack.com/services/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'
-    data = {"text": "!!! Scan results !!! \n"+"Image: (test) "+imagetag+'-'+buildid+"\n"+message}
+    url = 'https://hooks.slack.com/services/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'
+    data = {"text": "!!! Scan results !!! \n"+"Image: (test/apachestruts) "+imagetag+'-'+buildid+"\n"+message}
     headers = {'Content-Type': 'application/json'}
 
     try:
@@ -88,55 +75,6 @@ def createWebHook():
         sys.exit(1)
     return response.json()['hookUrl']
 
-def writeToJSONFile(path, fileName, data):
-    filePathNameWExt = './' + path + '/' + fileName + '.json'
-    with open(filePathNameWExt, 'w+') as fp:
-        json.dump(data, fp)
-
-def getVulnLayers(scanId):
-    print(scanId)
-    k=0
-    dataVulnUn = ""
-    message = ""
-    requests.packages.urllib3.disable_warnings()
-    url = "https://a0def78f6bfa011e99b471244930895f-1322482254.us-east-1.elb.amazonaws.com/api/scans/"
-    headers = {'Authorization': 'Bearer'+requestToken(), 'X-API-Version': '2018-05-01'}
-    querystring = {"id": scanId,"expand":"all"}
-    try:
-        response = requests.request("GET", url, headers=headers, params=querystring, verify=False)
-        lastScan = len(response.json()['scans'])-1
-        dataTmp = response.json()['scans'][lastScan]['details']['results'];
-
-        writeToJSONFile('./','datatotal',dataTmp)
-    except requests.exceptions.RequestException as e:
-        print (e)
-        sys.exit(1)
-    for dataT in dataTmp:
-        writeToJSONFile('./','datatotal',dataT['findings']['vulnerabilities']['total'])
-        if len(dataT['findings']['vulnerabilities']['total']) > 0:
-                layerID = dataT['id']
-                url = "https://a0def78f6bfa011e99b471244930895f-1322482254.us-east-1.elb.amazonaws.com/api/scans/"+scanId+"/layers/"+layerID+"/vulnerabilities/"
-                headers = {'Authorization': 'Bearer'+requestToken(), 'X-API-Version': '2018-05-01'}
-                querystring = {"expand":"all"}
-                try:
-                    response = requests.request("GET", url, headers=headers, params=querystring, verify=False)
-                    dataVulnUn = ""
-                    vulnerabilitiesForLayer=response.json()['vulnerabilities']
-                    if(len(response.json()['vulnerabilities']) > 0):
-                        print(len(response.json()['vulnerabilities']))
-                        vulnerabilities = response.json()['vulnerabilities'][0]['vulnerabilities']
-                        message += response.json()['vulnerabilities'][0]['name'] + ": \n"
-                        for value in vulnerabilities:
-                            if value['severity'] == 'high':
-                                k+=1
-                                dataVulnUn += value['name'] + ".\n"
-                                message += dataVulnUn +";"
-                                #writeToJSONFile('./','datatotal {}'.format(k),response.json())
-                    sendToSlack(message)
-                except requests.exceptions.RequestException as e:
-                    print (e)
-                    sys.exit(1)
-
 def requestReport():
     requests.packages.urllib3.disable_warnings()
     high, medium, low, negligible, unknown = 0, 0, 0, 0, 0
@@ -144,7 +82,7 @@ def requestReport():
 
     url = "https://a0def78f6bfa011e99b471244930895f-1322482254.us-east-1.elb.amazonaws.com/api/scans/"
     headers = {'Authorization': 'Bearer'+requestToken(), 'X-API-Version': '2018-05-01'}
-    querystring = {"id": requestScan(),"expand":"all"}
+    querystring = {"id": requestScan(),"expand":"none"}
 
     while status != "completed-with-findings":
         try:
@@ -163,14 +101,11 @@ def requestReport():
             sys.exit(1)
 
     data = response.json()
-    print(len(data['scans'][0]['details']['results']))
-    getVulnLayers(data['scans'][0]['details']['results'],data['scans'][0]['id'])
 
     if(status == "completed-with-findings" ):
         findings = data['scans'][0]['findings']
         vulnerabilities = findings['vulnerabilities']
-        #print(data['scans'][0]['details']['results'][0])
-        #layers.append(data['scans'][0]['details']['results'][0])
+
         dataVuln = "Vulnerabilities found: \n"
         dataMalw = ""
 
@@ -206,4 +141,4 @@ def requestReport():
 
     sendToSlack(message)
 
-getVulnLayers('002653a1-0406-495e-8b11-7eb25fa20c38')
+requestReport()
