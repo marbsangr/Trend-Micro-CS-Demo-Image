b6e4f42e102a06ed653b9dbf88e2c4523e599382
XeniaP <phoebe93xenia@gmail.com>
2021-02-04 01:24:14 -0600

    Update scAPI.py

--- scAPI.py
+++ scAPI.py
@@ -1,8 +1,11 @@
+""" Script Simple """
 import requests
 import json
 import os
 import sys
- 
+import urllib3
+import curlify
+
 #environmental variables
 imagetag=os.environ.get("IMAGETAG")
 buildid=os.environ.get("BUILD_ID")
@@ -13,85 +16,144 @@ negligible_t=os.environ.get("NEGLIGIBLE")
 unknown_t=os.environ.get("UNKNOWN")
 user=os.environ.get("USER")
 password=os.environ.get("PASSWORD")
- 
+
+smartCheckLB = os.environ.get("SC_HOSTNAME")
+userSC = os.environ.get("USER")
+passSC = os.environ.get("PASSWORD")
+
 def requestToken():
-    url = "https://ec2-34-232-77-93.compute-1.amazonaws.com:30112/api/sessions"
-    headers = {'Content-Type': 'application/json'}
-    data = {'user': {'userID': user, 'password': password}}
- 
+    requests.packages.urllib3.disable_warnings()
+    """ Request Session Token this is necesary for User Autentication """
+
+    url = "https://"+smartCheckLB+"/api/sessions"
+    headers = {'Content-Type': 'application/json', 'X-API-Version': '2018-05-01' }
+    data = {'user': {'userID': userSC, 'password': passSC }}
+
     try:
         response = requests.request("POST", url, json=data, headers=headers, verify=False)
+        print(curlify.to_curl(response.request))
+        print(requests.request("POST", url, json=data, headers=headers, verify=False))
     except requests.exceptions.RequestException as e:
         print (e)
         sys.exit(1)
- 
     return response.json()['token']
- 
-def requestScan():
-    url = "https://ec2-34-232-77-93.compute-1.amazonaws.com:30112/api/scans"
-    data = {"source": {
-        "type": "docker",
-        "registry": "846753579733.dkr.ecr.us-east-1.amazonaws.com",
-        "repository": "xeniarepo",
-        "tag": buildid,
-        "credentials": {"aws": {"region": "us-east-1"}}},
-        "webhooks": [{
-        "hookURL": "https://outlook.office.com/webhook/655e2408-31ef-4e6d-b1d2-e265ebeafc78@3e04753a-ae5b-42d4-a86d-d6f05460f9e4/IncomingWebhook/58a46e4e6b934c66ae7f5f8ad0d8fed6/9a94ffd6-c8f3-4c5c-98b0-d7a179e8c651"}]}
-    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer'+requestToken()}
- 
+
+def listSessions():
+    requests.packages.urllib3.disable_warnings()
+    """ Request Session Token this is necesary for User Autentication """
+
+    url = "https://"+smartCheckLB+"/api/sessions"
+    headers = {'Content-Type': 'application/json', 'X-API-Version': '2018-05-01' }
+    data = {'user': {'userID': userSC, 'password': passSC },'expand': 'all', 'limit':'25'}
+
     try:
         response = requests.request("POST", url, json=data, headers=headers, verify=False)
+        print(curlify.to_curl(response.request))
         print(response.json())
     except requests.exceptions.RequestException as e:
         print (e)
         sys.exit(1)
- 
+
+def requestScan():
+    requests.packages.urllib3.disable_warnings()
+    url = "https://"+smartCheckLB+"/api/scans"
+    data = {"source": {
+        "type": "docker",
+        "registry": "",
+        "repository": "dev/django",
+        "tag": 'latest',
+        "credentials": {"aws": {"region": "us-east-2"}}},
+        "webhooks": [{
+        "hookURL": createWebHook()}]}
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer'+requestToken(), 'X-API-Version': '2018-05-01'}
+    try:
+        response = requests.request("POST", url, json=data, headers=headers, verify=False)
+        print(curlify.to_curl(response.request))
+    except requests.exceptions.RequestException as e:
+        print (e)
+        sys.exit(1)
     return response.json()['id']
- 
-def sendToSlack(message):
-    url = 'https://outlook.office.com/webhook/655e2408-31ef-4e6d-b1d2-e265ebeafc78@3e04753a-ae5b-42d4-a86d-d6f05460f9e4/IncomingWebhook/58a46e4e6b934c66ae7f5f8ad0d8fed6/9a94ffd6-c8f3-4c5c-98b0-d7a179e8c651'
-    data = {"text": "!!! Scan results !!! \n"+"Image: "+imagetag+'-'+buildid+"\n"+message}
+
+def listScan():
+    requests.packages.urllib3.disable_warnings()
+    url = "https://"+smartCheckLB+"/api/scans/"
+    headers = {'Authorization': 'Bearer'+requestToken(), 'X-API-Version': '2018-05-01'}
+    querystring = {"expand":"all", "status":"completed-with-findings"}
+
+    try:
+        response=requests.request("GET", url, headers=headers,params=querystring,verify=False)
+        data = response.json()
+        obj = open("test.txt", "wb")
+        obj.write(json.dumps(data))
+        obj.close()
+        print (json.dumps(data))
+    except requests.exceptions.RequestException as e:
+        print (e)
+        sys.exit(1)
+
+
+def sendToSlack(message, data):
+    url = 'https://hooks.slack.com/services/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'
     headers = {'Content-Type': 'application/json'}
- 
+
     try:
         response = requests.request("POST", url, json=data, headers=headers)
     except requests.exceptions.RequestException as e:
         print (e)
         sys.exit(1)
- 
+
+def createWebHook():
+    requests.packages.urllib3.disable_warnings()
+    url = "https://"+smartCheckLB+"/api/webhooks"
+    data = { "name": "Test WebHook descriptive string",
+              "hookURL": "https://"+smartCheckLB+"/",
+              "secret": "tHiSiSaBaDsEcReT",
+              "events": [
+                "scan-requested"
+              ]
+            }
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer'+requestToken()}
+    try:
+        response = requests.request("POST", url, json=data, headers=headers, verify=False)
+    except requests.exceptions.RequestException as e:
+        print (e)
+        sys.exit(1)
+    return response.json()['hookUrl']
+
 def requestReport():
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     high, medium, low, negligible, unknown = 0, 0, 0, 0, 0
     status='pending'
- 
-    url = "https://ec2-34-232-77-93.compute-1.amazonaws.com:30112/api/scans/"
-    headers = {'Authorization': 'Bearer'+requestToken()}
+
+    url = "https://"+smartCheckLB+"/api/scans/"
+    headers = {'Authorization': 'Bearer'+requestToken(), 'X-API-Version': '2018-05-01'}
     querystring = {"id": requestScan(),"expand":"none"}
- 
+
     while status != "completed-with-findings":
         try:
             response=requests.request("GET", url, headers=headers,params=querystring,verify=False)
         except requests.exceptions.RequestException as e:
             print (e)
             sys.exit(1)
- 
+
         status = response.json()['scans'][0]['status']
-         
+
         if (status == "completed-no-findings"):
             break
- 
+
         if status == 'failed':
             print("Scan failed!")
             sys.exit(1)
- 
+
     data = response.json()
- 
+
     if(status == "completed-with-findings" ):
         findings = data['scans'][0]['findings']
         vulnerabilities = findings['vulnerabilities']
- 
+
         dataVuln = "Vulnerabilities found: \n"
         dataMalw = ""
- 
+
         for value in vulnerabilities['total']:
             if value == 'high':
                 high = vulnerabilities['total']['high']
@@ -108,20 +170,22 @@ def requestReport():
             if value == 'unknown':
                 unknown = vulnerabilities['total']['unknown']
                 dataVuln = dataVuln+"Unknown: "+str(unknown)+"\n"
-         
+
         if dataVuln == "Vulnerabilities found: \n": dataVuln=""
- 
+
         for value in findings:
             if value == 'malware':
                 malware = findings['malware']
                 dataMalw = "Malware found: "+str(malware)
- 
+
         message = dataVuln+dataMalw
- 
+
     if (high <= int(high_t)) and (medium <= int(medium_t)) and (low <= int(low_t)) and (negligible <= int(negligible_t)) and (unknown <= int(unknown_t) and (malware < 1)):
         sys.stdout.write('1')
         message = "Image is clean and ready to be deployed!"
- 
+
+    data = {"text": "!!! Scan results !!! \n"+"Image: "+imagetag+'-'+buildid+"\n"+message}
+
     sendToSlack(message)
- 
-requestReport()
+
+listSessions()
